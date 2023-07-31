8229378e001bc1f6df0feab58dd073f61efc3fdb
hyun <h.zoo3963>
2023-07-31 21:24:29 +0900

    init commit

---
+++ src/main/java/com/sparta/i_mu/IMuApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.i_mu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IMuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IMuApplication.class, args);
+    }
+
+}
