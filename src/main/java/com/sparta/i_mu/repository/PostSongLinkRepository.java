697222975ff6da24fdd68bdc321a294bde37c05a
hyun <h.zoo3963>
2023-08-02 17:19:42 +0900

    feat: "PostSongLink 생성"
    body: Post와 Song의 연결 테이블 생성

---
+++ src/main/java/com/sparta/i_mu/repository/PostSongLinkRepository.java
@@ -0,0 +1,6 @@
+package com.sparta.i_mu.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostSongLinkRepository extends JpaRepository <PostSongLinkRepository, Long> {
+}
