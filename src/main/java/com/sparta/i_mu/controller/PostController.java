39d0e2c3b670ca1e8dc1b37182bd4492e8d34043
hyun <h.zoo3963>
2023-08-03 15:46:39 +0900

    feat: "게시글 삭제 기능 생성"
    body:
    1. 게시글 삭제 기능 생성
    2. 삭제할 게시물의 존재와 사용자의 권한을 확인하는 메서드를 분리

--- src/main/java/com/sparta/i_mu/controller/PostController.java
+++ src/main/java/com/sparta/i_mu/controller/PostController.java
@@ -6,22 +6,23 @@ import com.sparta.i_mu.dto.responseDto.PostResponseDto;
 import com.sparta.i_mu.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-// 현재 위치에 해당하는 노래 조회 (게시물에서 노래를 따로 빼야하나 -> songcontroller?)
-// 주변 게시글 전체 조회
+// 주변 게시글 카테고리별 전체 조회 -> O
 // 상세 게시글 조회
-// 카테고리 별 게시글 조회
-// 게시글 작성
-// 게시글 수정
-// 게시글 삭제
+// 게시글 작성 -> O
+// 게시글 수정 -> O
+// 게시글 삭제 -> O
 // 좋아요 순 게시글 조회
 // 좋아요 순 반대 게시글 조회
 // 최근 순 게시글 조회
+
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/api/posts")
@@ -31,15 +32,44 @@ public class PostController {
 
     // 게시물 등록
     @PostMapping
-    public ResponseEntity<?> createPost(@RequestBody PostSaveRequestDto postRequestDto) {
-        return postService.createPost(postRequestDto);
-
+    public ResponseEntity<?> createPost(
+            @RequestBody PostSaveRequestDto postRequestDto
+            @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        User user = userDetails.getUser();
+        return postService.createPost(postRequestDto,user);
     }
+
+
+
+    @GetMapping("/")
+    // 게시글 수정
+    @PutMapping("/{postId}")
+    public ResponseEntity<?> updatePost(
+            @PathVariable Long postId,
+            @RequestBody PostSaveRequestDto postRequestDto
+            @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+        User user = userDetails.getUser();
+        return postService.updatePost(postId,postRequestDto,user);
+    }
+
+    // 게시물 삭제
+    @DeleteMapping("/{postId}")
+    public ResponseEntity<?> deletePost(
+            @PathVariable Long postId,
+            @AuthenticationPrincipal UserDetailsImpl userDetails){
+        User user = userDetails.getUser();
+
+        return postService.deletePost(postId,user);
+    }
+
+
     // 위치 서비스에 따른 카테고리별 게시글 조회
     @Transactional(readOnly = true)
     @GetMapping
-    public List<PostResponseDto> getPostByCategory(@RequestBody PostSearchRequestDto postSearchRequestDto){
+    public List<?> getPostByCategory(
+            @RequestBody PostSearchRequestDto postSearchRequestDto){
         return postService.getPostByCategory(postSearchRequestDto);
     }
-    // 게시물 수정
+
 }
--- src/main/java/com/sparta/i_mu/service/PostService.java
+++ src/main/java/com/sparta/i_mu/service/PostService.java
@@ -2,33 +2,30 @@ package com.sparta.i_mu.service;
 
 import com.sparta.i_mu.dto.requestDto.PostSaveRequestDto;
 import com.sparta.i_mu.dto.requestDto.PostSearchRequestDto;
+import com.sparta.i_mu.dto.responseDto.PostResponseDto;
 import com.sparta.i_mu.entity.Location;
 import com.sparta.i_mu.entity.Post;
 import com.sparta.i_mu.entity.Song;
 import com.sparta.i_mu.entity.User;
-import com.sparta.i_mu.repository.LocationRepository;
-import com.sparta.i_mu.repository.PostRepository;
-import com.sparta.i_mu.repository.PostSongLinkRepository;
-import com.sparta.i_mu.repository.SongRepository;
+import com.sparta.i_mu.repository.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.nio.file.AccessDeniedException;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static com.sparta.i_mu.mapper.PostMapper.POST_INSTANCE;
 
-// 현재 위치에 해당하는 노래 조회
-// 주변 게시글 전체 조회
-// 상세 게시글 조회
-// 카테고리 별 게시글 조회
+// 주변 게시글 카테고리별 전체 조회 -> O
+// 상세 게시글 조회 -> O
 // 게시글 작성 -> O
-// 게시글 수정
-// 게시글 삭제
+// 게시글 수정 -> O
+// 게시글 삭제 -> O
 // 좋아요 순 게시글 조회
 // 좋아요 순 반대 게시글 조회
 // 최근 순 게시글 조회
@@ -40,13 +37,14 @@ public class PostService {
     private PostRepository postRepository;
     private SongRepository songRepository;
     private PostSongLinkRepository postSongLinkRepository;
+    private WishlistRepository wishlistRepository;
     private LocationRepository locationRepository;
     private static final Double DISTANCE_IN_METERS = 500.0;
 
     /**
-     * post우선 생성후 lccation, Songs들을 주입
+     * post 생성 메서드
      * @param postRequestDto
-     * @return 완료 메세지
+     * @return 완료 응답 메세지
      */
     public ResponseEntity<?> createPost(PostSaveRequestDto postRequestDto, User user) {
         // user가 null이 아닐 경우에만 게시글을 작성
@@ -85,7 +83,6 @@ public class PostService {
 
     /**
      * 위치 정보를 동의 여부에 따른 카테고리 별 게시물 조회서비스
-     * TODO 동의하지 않았을 경우 기본위치에 관한 카테고리를 보여주게 수정필요
      * @param postSearchRequestDto
      * @return 근처에 해당하는 카테고리별 게시물들
      */
@@ -95,13 +92,12 @@ public class PostService {
         String category = postSearchRequestDto.getCategory();
         Double longitude = postSearchRequestDto.getLocation().getLongitude();
         Double latitude = postSearchRequestDto.getLocation().getLatitude();
-
-        if(!postSearchRequestDto.getLocationAgreed()) {
-            return Collections.singletonList(ResponseEntity.status(HttpStatus.FORBIDDEN).body("위치정보에 동의하신다면 더 많은 서비스를 이용하실 수 있습니다."));
-        }
+//        if(!postSearchRequestDto.getLocationAgreed()) {
+//            return Collections.singletonList(ResponseEntity.status(HttpStatus.FORBIDDEN).body("위치정보에 동의하신다면 더 많은 서비스를 이용하실 수 있습니다."));
+//        }
         List<Post> posts = postRepository.findAllByCategoryAndLocationNear(category, latitude, longitude, DISTANCE_IN_METERS);
         return posts.stream()
-                .map(POST_INSTANCE::entityToDto)
+                .map(this::mapToPostResponseDto)
                 .collect(Collectors.toList());
     }
 
@@ -112,15 +108,11 @@ public class PostService {
      * @return 수정된 게시글
      */
     @Transactional
-    public ResponseEntity<?> updatePost(Long postId, PostSaveRequestDto postRequestDto, User user) {
-
-        Post post = postRepository.findById(postId)
-                .orElseThrow(()-> new IllegalArgumentException("해당 게시물이 없습니다."));
-
+    public ResponseEntity<?> updatePost(Long postId, PostSaveRequestDto postRequestDto, User user) throws AccessDeniedException {
+        // post 존재 여부 확인
+        Post post = findPost(postId);
         // 사용자 확인
-        if(!post.getUser().getId().equals(user.getId())){
-            throw new IllegalArgumentException("해당 게시물의 작성자만 수정이 가능합니다.");
-        }
+        checkAuthority(post, user);
 
         // post의 song의 연결 주체인 postSongLink 삭제
         post.removeSongs();
@@ -142,5 +134,41 @@ public class PostService {
 
         return ResponseEntity.status(HttpStatus.OK).body("게시물이 업데이트 되었습니다.");
     }
+    /**
+     * 게시글 삭제
+     * @param postId
+     * @param user
+     * @return 게시글 삭제가 완료되었습니다 응답 메시지
+     * @throws AccessDeniedException
+     */
+    public ResponseEntity<?> deletePost(Long postId, User user) throws AccessDeniedException {
 
+        Post post = findPost(postId);
+        checkAuthority(post,user);
+        postRepository.delete(post);
+        return ResponseEntity.status(HttpStatus.OK).body("해당 게시글 삭제를 완료하였습니다.");
+    }
+
+
+    // 수정, 삭제 할 게시물이 존재하는지 확인하는 메서드
+    public Post findPost(Long postId) {
+        return postRepository.findById(postId).orElseThrow(() ->
+                new NullPointerException("존재하지 않는 게시글입니다."));
+    }
+
+    // 수정, 삭제 할 게시물의 권한을 확인하는 메서드
+
+    public void checkAuthority(Post post, User user) throws AccessDeniedException {
+//        // admin 확인
+//        if (!user.getRole().getAuthority().equals("ROLE_ADMIN")) {
+//            // userId 확인
+//            if (post.getUser().getId() != user.getId()) {
+//                throw new AccessDeniedException("작성자만 수정, 삭제가 가능합니다.");
+//            }
+//        }
+
+        if (post.getUser().getId().equals(user.getId())){
+            throw new AccessDeniedException("작성자만 수정, 삭제가 가능합니다.");
+        }
+    }
 }
