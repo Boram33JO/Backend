45be7d4bfac2713fa3aa1e143e01275297923539
hyun <h.zoo3963>
2023-08-05 10:45:06 +0900

    feat: "postMapper에서 song 매핑을 위한 jpa"

--- src/main/java/com/sparta/i_mu/repository/PostSongLinkRepository.java
+++ src/main/java/com/sparta/i_mu/repository/PostSongLinkRepository.java
@@ -3,5 +3,8 @@ package com.sparta.i_mu.repository;
 import com.sparta.i_mu.entity.PostSongLink;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostSongLinkRepository extends JpaRepository <PostSongLink, Long> {
+    List<PostSongLink> findAllByPostId(Long id);
 }
