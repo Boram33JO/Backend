1896705f642835bb6202adffef9d466cd80f98da
mabyoungg <mabyoungg@gmail.com>
2023-08-15 20:00:28 +0900

    remove: 불필요한 소셜 로그인 관련 파일 삭제

--- src/main/java/com/sparta/i_mu/kakao/jwt/JwtProperties.java
+++
@@ -1,9 +0,0 @@
-//package com.sparta.i_mu.kakao.jwt;
-//
-//
-//public interface JwtProperties { //(1)
-//    String SECRET = "7Iqk7YyM66W07YOA7L2U65Sp7YG065+9U3ByaW5n6rCV7J2Y7Yqc7YSw7LWc7JuQ67mI7J6F64uI64ukLg=="; //(2)
-//    int EXPIRATION_TIME =  864000000; //(3)
-//    String TOKEN_PREFIX = "Bearer "; //(4)
-//    String HEADER_STRING = "Authorization"; //(5)
-//}
