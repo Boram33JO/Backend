127062649b4a74f1f35476c68476e59cb5579a26
lsm <deadmouse210@naver.com>
2023-08-23 14:48:13 +0900

    feat:smtp

--- src/main/java/com/sparta/i_mu/global/util/RedisUtil.java
+++ src/main/java/com/sparta/i_mu/global/util/RedisUtil.java
@@ -1,10 +1,13 @@
 package com.sparta.i_mu.global.util;
 
+import com.sparta.i_mu.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
+
+import java.time.Duration;
 
 @Component
 @RequiredArgsConstructor
@@ -12,6 +15,7 @@ import org.springframework.stereotype.Service;
 public class RedisUtil {
     private final RedisTemplate<String, String> redisTemplate;
     private static final String REFRESH_TOKEN_KEY = "REFRESH_TOKEN_";
+    private final UserRepository userRepository;
 
     public void storeRefreshToken(String accessToken, String refreshToken) {
         redisTemplate.opsForValue().set(REFRESH_TOKEN_KEY + accessToken, refreshToken);
@@ -21,7 +25,23 @@ public class RedisUtil {
         return (String) redisTemplate.opsForValue().get(REFRESH_TOKEN_KEY + accessToken);
     }
 
+    public String getData(String email) {
+        return (String) redisTemplate.opsForValue().get(email);
+    }
+
     public void removeRefreshToken(String accessToken) {
         redisTemplate.delete(REFRESH_TOKEN_KEY + accessToken);
+    }
+
+    public void removeData(String email) {
+        redisTemplate.delete(email);
+    }
+
+
+    //유효 시간 동안만 저장
+    public void setDataExpire(String key, String value, long duration) {
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        Duration expireDuration = Duration.ofSeconds(duration);
+        valueOperations.set(key, value, expireDuration);
     }
 }
