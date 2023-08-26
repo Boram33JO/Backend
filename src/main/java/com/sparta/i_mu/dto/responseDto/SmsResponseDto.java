a79b43c1f29d0898333de9ee5df7377633f03e1b
lsm <deadmouse210@naver.com>
2023-08-26 21:27:03 +0900

    feat:SMS

---
+++ src/main/java/com/sparta/i_mu/dto/responseDto/SmsResponseDto.java
@@ -0,0 +1,25 @@
+package com.sparta.i_mu.dto.responseDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+public class SmsResponseDto {
+    private String requestId;
+    private LocalDateTime requestTime;
+    private String statusCode;
+    private String statusName;
+    private String smsConfirmNum;
+
+    //sms
+    public SmsResponseDto(String smsConfirmNum) {
+        this.smsConfirmNum = smsConfirmNum;
+    }
+}
