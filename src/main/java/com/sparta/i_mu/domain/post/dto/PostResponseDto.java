a4022dacc2e205f2275b7302564bfbfce7ac3416
mabyoungg <mabyoungg@gmail.com>
2023-09-18 00:40:23 +0900

    fix: LocalDateTime deserialize error fix

--- src/main/java/com/sparta/i_mu/domain/post/dto/PostResponseDto.java
+++ src/main/java/com/sparta/i_mu/domain/post/dto/PostResponseDto.java
@@ -1,5 +1,9 @@
 package com.sparta.i_mu.domain.post.dto;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.sparta.i_mu.domain.comment.dto.CommentResponseDto;
 import com.sparta.i_mu.domain.song.dto.SongResponseDto;
 import com.sparta.i_mu.domain.location.entity.Location;
@@ -21,9 +25,19 @@ public class PostResponseDto {
     private String userImage;
     private String content;
     private Long category;
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime createdAt;
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime modifiedAt;
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime deletedAt;
+
     private Boolean deleted;
     private Boolean wishlist;
     private Boolean follow;
--- src/main/java/com/sparta/i_mu/global/util/Timestamped.java
+++ src/main/java/com/sparta/i_mu/global/util/Timestamped.java
@@ -1,5 +1,9 @@
 package com.sparta.i_mu.global.util;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
@@ -21,14 +25,20 @@ public abstract class Timestamped {
     @CreatedDate
     @Column(updatable = false)
     @Temporal(TIMESTAMP)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
     @Column
     @Temporal(TIMESTAMP)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private  LocalDateTime modifiedAt;
 
     @Column
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime deleteAt;
 
     public void setDeletedAt(LocalDateTime now) {
